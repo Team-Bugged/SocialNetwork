53aef7ce00d83e5e94085c8f635ff14cf821d20a
Sahil <sahilflash123@gmail.com>
2022-04-14 19:50:47 +0530

    Fixed authenticate middleware & new getuserdetail endpoint

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-function authenticate (req, res, next){
+const authenticate  = (req, res, next)=> {
     const authHeader = req.headers['authorization'];
     const token = authHeader  && authHeader.split(' ')[1];
 
@@ -19,10 +19,15 @@ function authenticate (req, res, next){
         return res.sendStatus(401);
 
     jwt.verify(token, process.env.SECRET_KEY, (err, user)=>{
-        console.log(user);
+        if(err)
+            res.send(err);
+
+        req.username = user;
         next();
     })
 }
+
+// app.use(authenticate);
 
 
 const driver = neo4j.driver(
@@ -30,7 +35,7 @@ const driver = neo4j.driver(
     neo4j.auth.basic(process.env.DB_NAME, process.env.DB_PASSWORD)
 );
 
-app.get("/", authenticate, (req, res)=>{
+app.get("/", (req, res)=>{
         res.send("Hello world");
 })
 
@@ -39,42 +44,97 @@ app.post("/register", (req, res) =>{
     let email = req.body.email;
     let password = req.body.password;
 
-    // let session = driver.session();
-    var session = driver.session();
+    let session = driver.session();
 
-    // the Promise way, where the complete result is collected before we act on it:
     session
-    .run('CREATE (:User {username: $usernameP, email: $emailP, password: $passwordP})',{
-        usernameP: username,
-        emailP: email,
-        passwordP: password
-    })
-    .then(result => {
-            console.log(result)    
-        // result.records.forEach(record => {
-            // console.log(record.get('name'))
+    .run('MATCH (u:User {username: $usernameP}) RETURN u', {
+        usernameP: username})
+    .then((result)=>{
+        //if already present conflict status 409
+        if(result.records.length>0){
+            res.status(409);
+            res.send({meassage: "username already exists"});
+            return;
+        }
+        // session.close();
+        // session = driver.session();
+        session.run('CREATE (:User {username: $usernameP, email: $emailP, password: $passwordP})',{
+            usernameP: username,
+            emailP: email,
+            passwordP: password
         })
-    .catch(error => {
-        console.log(error)
+        .then(result => {
+                console.log(result)    
+                res.status(200);
+                res.send({"message": "Successfully Registered"});
+            })
+        .catch(error => {
+            console.log(error)
+        })
+        .then(()=>{
+            session.close();
+        })
     })
-    .then(() => session.close());
+    .catch();
 })
 
 app.post("/login", (req, res)=>{
     let username = req.body.username;
     let password = req.body.password;
     
-    if(password!="123"){
+    let session = driver.session();
 
-        /*USERNAME & PASSWORD CHECK GOES HERE*/
-        res.status(403);
-        res.send();
-        return;
-    }
+    session.run('MATCH (u:User {username: $usernameP}) RETURN u',{
+        usernameP: req.username,
+    })
+    .then((result)=>{
+        if(result.records.length>0){
+            let props =  result.records[0]._fields[0].properties;
+            if(props.password == password){
+                const TOKEN = jwt.sign(username, process.env.SECRET_KEY);
+                res.json({token: TOKEN});    
+            }
+            else{
+                res.status(401);
+                res.send({message: "Unauthorized access"});
+            }
+            
+        }
+        else{
+            res.status(404);
+            res.send({message: "Username not found do register"});
+        }
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.send(err);
+    })
+    .then(()=>{
+        session.close();
+    })
 
-    const TOKEN = jwt.sign(userName, process.env.SECRET_KEY);
-    res.json({token: TOKEN});
+   
 })  
+
+app.get("/getuserdetails",authenticate, (req, res)=> {
+    let session = driver.session();
+
+    session.run('MATCH (u:User {username: $usernameP}) RETURN u', {
+        usernameP: req.username,
+    })
+    .then((result)=>{
+        console.log(result);
+        res.status(200);
+        res.send(result.records[0]._fields[0].properties);
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.send({message: err});
+    })
+    .then(()=>{
+        session.close();
+    })
+})
 
 app.get("/close", (req, res)=>{
     driver.close();
