98206c090429a59038af1bcda99f8a9cb0795be0
siddharthsharma19 <sharmasiddharth017@gmail.com>
2022-06-22 10:05:12 +0530

    Implement incomingConnections, getSuggestions and acceptConnection in client

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,25 @@ import { Login } from "./Login";
 import { Register } from "./Register";
 import { Home } from "./Home";
 import { UserPage } from "./UserPage";
-import {Profile} from "./Profile";
+import { Profile } from "./Profile";
+import { IncomingConnections } from "./incomingConnections";
+import { GetSuggestions } from "./getSuggestions";
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element = {<Welcome/>}/>
-      <Route exact path="/login" element = {<Login/>}/>
-      <Route exact path="/register" element = {<Register/>}/>
-      <Route exact path="/home" element = {<Home/>}/>
-      <Route exact path="/user/:username" element = {<UserPage/>}/>
-      <Route exact path="/profile" element={<Profile/>}/>
+      <Route exact path="/" element={<Welcome />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/register" element={<Register />} />
+      <Route exact path="/home" element={<Home />} />
+      <Route exact path="/user/:username" element={<UserPage />} />
+      <Route exact path="/profile" element={<Profile />} />
+      <Route
+        exact
+        path="/incomingConnections"
+        element={<IncomingConnections />}
+      />
+      <Route exact path="/getSuggestions" element={<GetSuggestions />} />
     </Routes>
   );
 }
--- client/src/Login.js
+++ client/src/Login.js
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 import Cookies from "universal-cookie";
 
 const Login = () => {
-
   const [username, setusername] = useState("");
   const [password, setPassword] = useState("");
   const [disabledFlag, setDisabledFlag] = useState(true);
@@ -22,13 +21,12 @@ const Login = () => {
 
   const handleLoginSubmit = (event) => {
     event.preventDefault();
-        loginUser(username, password)
-        .then((response)=>{
-          if(response.status===200){
-            cookies.set('token', response.data.token, {maxAge: 3600});
-          }
-          navigate("/home");
-        })
+    loginUser(username, password).then((response) => {
+      if (response.status === 200) {
+        cookies.set("token", response.data.token, { maxAge: 3600 });
+      }
+      navigate("/home");
+    });
   };
 
   useEffect(() => {
@@ -38,50 +36,47 @@ const Login = () => {
     }
   }, [username, password]);
 
-  
-
   return (
     <>
-
-            <div className="login-header">
-              <h3>Sign In</h3>
-              <p>Please enter your credentials to signin.</p>
-            </div>
-            <form className="login-form">
-              <div>
-                <label for="username">
-                  <b>username</b>
-                </label>
-                <br />
-                <input
-                  type="text"
-                  placeholder="username"
-                  name="username"
-                  onChange={handleusernameChange}
-                  required
-                />
-                <br />
-                <label for="password">
-                  <b>Password</b>
-                </label>
-                <br />
-                <input
-                  type="password"
-                  placeholder="password"
-                  name="password"
-                  onChange={handlePasswordChange}
-                  required
-                />
-                <br />
-                <button
-                  className="signin-btn"
-                  disabled={disabledFlag}
-                  onClick={handleLoginSubmit}
-                >
-                  Login
-                </button>
-              </div>
-            </form>
+      <div className="login-header">
+        <h3>Sign In</h3>
+        <p>Please enter your credentials to signin.</p>
+      </div>
+      <form className="login-form">
+        <div>
+          <label for="username">
+            <b>username</b>
+          </label>
+          <br />
+          <input
+            type="text"
+            placeholder="username"
+            name="username"
+            onChange={handleusernameChange}
+            required
+          />
+          <br />
+          <label for="password">
+            <b>Password</b>
+          </label>
+          <br />
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            onChange={handlePasswordChange}
+            required
+          />
+          <br />
+          <button
+            className="signin-btn"
+            disabled={disabledFlag}
+            onClick={handleLoginSubmit}
+          >
+            Login
+          </button>
+        </div>
+      </form>
     </>
   );
 };
